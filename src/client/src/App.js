d5ca20bbf6eb6b2ee4759f393e8b7f199315a071
alexispiret <alexis.piret@efrei.net>
2024-06-20 17:26:56 +0200

    map

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,23 +1,40 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
+  const position = [46.603354, 1.888334]; // Coordonnées centrales de la France
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='navbar-home'>
+        <ul className="navbar-list">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>À propos</a>
+          </li>
+          <li>
+            <a>Collaborer</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="map">
+        <MapContainer center={position} zoom={6} style={{ height: "600px", width: "100%" }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
   );
 }
