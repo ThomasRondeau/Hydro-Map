06d97f6515a1c426bd7e1bb0c18f9f218d4e74eb
alexispiret <alexis.piret@efrei.net>
2024-07-09 11:42:47 +0200

    décalage réglé

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -13,17 +13,18 @@ import additionalPointsData from './DataStationPotentiel15mLargeur.csv'
 const customIcon = L.icon({
   iconUrl: new_icon, 
   iconSize: [7, 7], 
-  iconAnchor: [19, 38], 
+  iconAnchor: [3, 3], 
   popupAnchor: [0, -38]
 });
 const customIcon2 = L.icon({
   iconUrl: new_icon2, 
-  iconSize: [30, 30], 
-  iconAnchor: [19, 38], 
+  iconSize: [7, 7], 
+  iconAnchor: [3, 3], 
   popupAnchor: [0, -38]
 });
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
   const [points, setPoints] = useState([]);
   const [additionalPoints, setadditionalPoints] = useState ([]);
   const [powerUnits, setPowerUnits] = useState([]);
@@ -137,7 +138,7 @@ useEffect(() => {
             return !isNaN(lat) && !isNaN(lng);
           });
           const filteredPoints = validPoints.filter((point, index) => index % 2 === 0); // pour réduire le lag
-          setPoints(filteredPoints.slice(0, 4000)); // nbr éléments affichés
+          setPoints(validPoints.slice(0, 4000)); // nbr éléments affichés
         },
         error: (error) => {
           console.error('Error parsing CSV:', error);
@@ -162,53 +163,66 @@ useEffect(() => {
         }
       });
     }, []);
-  return (
-    <div className="App">
-      <nav className="navbar-home">
+    const renderContent = () => {
+      switch (currentPage) {
+        case 'home':
+          return (
+            <div className="map">
+              <MapContainer center={position} zoom={6} style={{ height: "600px", width: "100%" }}>
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {points.map((point, index) => (
+                  <Marker key={index} position={[parseFloat(point.latitude), parseFloat(point.longitude)]} icon={customIcon}>
+                    <Popup>
+                      {point.nomInstallation}/ puissance : {point.puisMaxInstallee} MW
+                    </Popup>
+                  </Marker>
+                ))}
+                {additionalPoints.map((addipoint, index) => (
+                  <Marker key={index} position={[parseFloat(addipoint.CoordYAval), parseFloat(addipoint.CoordXAval)]} icon={customIcon2}>
+                    <Popup>
+                      {addipoint._id}
+                    </Popup>
+                  </Marker>
+                ))}
+              </MapContainer>
+            </div>
+          );
+        case 'about':
+          return <div>À propos : Cette application affiche les infrastructures hydroélectriques.</div>;
+        case 'collaborate':
+          return <div>Collaborer : Veuillez nous contacter pour des partenariats.</div>;
+        default:
+          return null;
+      }
+    };
+  
+    return (
+      <div className="App">
+        <nav className="navbar-home">
           <ul className="navbar-list">
             <li>
-              <a className="title_Site">Hydro Map</a>
+              <a className="title_Site" onClick={() => setCurrentPage('home')}>Hydro Map</a>
             </li>
             <li>
-              <a to="/">Home</a>
+              <a onClick={() => setCurrentPage('home')}>Home</a>
             </li>
             <li>
-              <a to="/about">À propos</a>
+              <a onClick={() => setCurrentPage('about')}>À propos</a>
             </li>
             <li>
-              <a to="/collaborate">Collaborer</a>
+              <a onClick={() => setCurrentPage('collaborate')}>Collaborer</a>
             </li>
           </ul>
-      </nav>
-      
-      <div className="map">
-        <MapContainer center={position} zoom={6} style={{ height: "600px", width: "100%" }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {points.map((point, index) => (
-            <Marker key={index} position={[parseFloat(point.latitude), parseFloat(point.longitude)]} icon={customIcon}>
-              <Popup>
-                {point.nomInstallation}/ puissance :
-                {point.puisMaxInstallee} MGW/H
-              </Popup>
-            </Marker>
-          ))}
-          {additionalPoints.map((addipoint, index) => (
-            <Marker key={index} position={[parseFloat(addipoint.CoordXAval), parseFloat(addipoint.CoordYAval)]} icon={customIcon2}>
-              <Popup>
-                {addipoint._id}
-              </Popup>
-            </Marker>
-          ))}
-        </MapContainer>
-      </div>
-      <footer className="footer">
+        </nav>
+        {renderContent()}
+        <footer className="footer">
           <p>&copy; 2024 Hydro Map. All rights reserved.</p>
-      </footer>
-    </div>
-  );
+        </footer>
+      </div>
+    );
 }
 
 export default App;
